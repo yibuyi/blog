a71d0d46fb49bcbff2732527b5174780ad34852c
一布衣 <li12qiang345@163.com>
2016-03-23 12:53:43 +0800

    “初次迁移”

---
+++ app/main/__init__.py
@@ -0,0 +1,11 @@
+from app.models import Permission
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+from . import views, errors
+
+
+@main.app_context_processor  # 把permission类加入模板上下文
+def inject_permissions():
+	return dict(Permission=Permission)
---
+++ manage.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# encoding=utf-8
+import os
+
+COV = None
+if os.environ.get('FLASKY_COVERAGE'):
+	import coverage
+
+	COV = coverage.coverage(branch=True, include='app/*')
+	COV.start()
+
+from app.models import Comment, User, Role, Permission, Follow, Post
+from app import create_app, db
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+# 回调函数，自动在shell导入特定对象
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role,
+				Follow=Follow, Permission=Permission, Post=Post, Comment=Comment)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+# 启动单元测试的命令
+
+
+
+@manager.command
+def test():
+	"""Run the unit tests."""
+	if coverage and not os.environ.get('FLASKY_COVERAGE'):
+		import sys
+		os.environ['FLASKY_COVERAGE'] = '1'
+		os.execvp(sys.executable, [sys.executable] + sys.argv)
+	import unittest
+	tests = unittest.TestLoader().discover('tests')
+	unittest.TextTestRunner(verbosity=2).run(tests)
+	if COV:
+		COV.stop()
+		COV.save()
+		print('Coverage Summary')
+		COV.report()
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		covdir = os.path.join(basedir, 'tmp/coverage')
+		COV.html_report(directory=covdir)
+		print('HTML version: file//%s/index.html' % covdir)
+		COV.erase()
+
+
+@manager.command
+def profile(length=25, profile_dir=None):
+	"""Start the application under the code profiler."""
+	from werkzeug.contrib.profiler import ProfilerMiddleware
+	app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+									  profile_dir=profile_dir)
+	app.run()
+
+
+@manager.command
+def deploy():  # 部署命令
+	"""Run deployment tasks."""
+	import flask_migrate
+	from app.models import Role, User
+
+	flask_migrate.upgrade()  # 数据库升级
+
+	Role.insert_roles()
+	User.add_self_follows()
+
+# 自动在浏览器刷新
+@manager.command
+def dev():
+	from livereload import Server
+	live_server = Server(app.wsgi_app)
+	live_server.watch('**/*.*')
+	live_server.serve(open_url=True)
+
+
+if __name__ == '__main__':
+	#manager.run()
+	app.run()
