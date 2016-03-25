f08fbf184f5271c109ef16096e264136e0a88570
一布衣 <li12qiang345@163.com>
2016-03-25 11:31:28 +0800

    gx

--- manage.py
+++ manage.py
@@ -5,12 +5,6 @@ from app import create_app, db
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-COV = None
-if os.environ.get('FLASKY_COVERAGE'):
-	import coverage
-
-	COV = coverage.coverage(branch=True, include='app/*')
-	COV.start()
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
@@ -27,10 +21,6 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def test():
 	"""Run the unit tests."""
-	if coverage and not os.environ.get('FLASKY_COVERAGE'):
-		import sys
-		os.environ['FLASKY_COVERAGE'] = '1'
-		os.execvp(sys.executable, [sys.executable] + sys.argv)
 	import unittest
 	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
