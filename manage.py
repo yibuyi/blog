3e5c28dd2d6212d7b616590a648a231f15d47a0a
一布衣 <li12qiang345@163.com>
2016-03-25 13:21:06 +0800

    gx

--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
-# encoding:utf-8
+# coding=utf-8
 import os
 from app.models import Comment, User, Role, Permission, Follow, Post
 from app import create_app, db
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+
+if os.path.exists('.env'):
+	print('Importing environment from .env...')
+	for line in open('.env'):
+		var = line.strip().split('=')
+		if len(var) == 2:
+			os.environ[var[0]] = var[1]
 
 app = create_app(os.getenv('FLASKY_CONFIG') or 'default')
 manager = Manager(app)
