2e55d8547b7736807a10d03da7258b0997d02a23
一布衣 <li12qiang345@163.com>
2016-03-25 12:39:16 +0800

    gx

--- manage.py
+++ manage.py
@@ -1,17 +1,17 @@
 # encoding=utf-8
 import os
+from app.models import Comment, User, Role, Permission, Follow, Post
 from app import create_app, db
-from app.models import User, Role, Permission, Post, Follow, Comment
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app(os.getenv('FLASKY_CONFIG') or 'default')
 manager = Manager(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app)
 
 
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post, Follow=Follow, Comment=Comment)
+	return dict(app=app, db=db, User=User, Role=Role, Follow=Follow, Permission=Permission, Post=Post, Comment=Comment)
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
@@ -20,23 +20,20 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
-	"""Run the unit tests."""
 	import unittest
 	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 @manager.command
-def deploy():  # 部署命令
-	"""Run deployment tasks."""
+def deploy():
 	import flask_migrate
 	from app.models import Role, User
-	flask_migrate.upgrade()  # 数据库升级
+	flask_migrate.upgrade()
 
 
 # Role.insert_roles()
 # User.add_self_follows()
-
 
 if __name__ == '__main__':
 	manager.run()
