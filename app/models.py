2b0c828849b29f1afa9a843f7cece3203673436c
一布衣 <li12qiang345@163.com>
2016-03-29 19:12:27 +0800

    删除虚拟用户和文章章”
    git commit -m 删除虚拟用户和文章章”

--- app/models.py
+++ app/models.py
@@ -50,6 +50,7 @@ class Role(db.Model):
 	def __repr__(self):
 		return '<Role %r>' % self.name
 
+
 # 关注关联表的模型实现
 class Follow(db.Model):
 	__tablename__ = 'follows'
@@ -79,30 +80,6 @@ class User(UserMixin, db.Model):
 								backref=db.backref('followed', lazy='joined'),
 								lazy='dynamic', cascade='all, delete-orphan')
 	comments = db.relationship('Comment', backref='author', lazy='dynamic')
-
-	@staticmethod  # 生成虚拟用户
-	def generate_fake(count=100):
-		from sqlalchemy.exc import IntegrityError
-		from random import seed
-		import forgery_py
-
-		seed()
-		for i in range(count):
-			u = User(
-				email=forgery_py.internet.email_address(),
-				username=forgery_py.internet.user_name(True),
-				password=forgery_py.lorem_ipsum.word(),
-				confirmed=True,
-				name=forgery_py.name.full_name(),
-				location=forgery_py.address.city(),
-				about_me=forgery_py.lorem_ipsum.sentence(),
-				member_since=forgery_py.date.date(True)
-			)
-			db.session.add(u)
-			try:
-				db.session.commit()
-			except IntegrityError:
-				db.session.rollback()
 
 	def __init__(self, **kwargs):  # 定义默认的用户角色
 		super(User, self).__init__(**kwargs)
@@ -262,22 +239,6 @@ class Post(db.Model):
 	author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 	body_html = db.Column(db.Text)
 	comments = db.relationship('Comment', backref='post', lazy='dynamic')
-
-	# 生成虚拟博客文章
-	@staticmethod
-	def generate_fake(count=100):
-		from random import seed, randint
-		import forgery_py
-
-		seed()
-		user_count = User.query.count()
-		for i in range(count):
-			u = User.query.offset(randint(0, user_count - 1)).first()
-			p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1, 3)),
-					 timestamp=forgery_py.date.date(True),
-					 author=u)
-			db.session.add(p)
-			db.session.commit()
 
 	def on_changed_body(target, value, oldvalue, initiator):
 		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
